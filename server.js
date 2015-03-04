769d02aafec73f7024de757cc358940f8f82ddab
Kevin Simper <kevin.simper@gmail.com>
2015-03-04 18:02:15 +0100

    get members of meetup

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var cheerio = require('cheerio');
+var request = require('request');
 
 var app = express();
 
@@ -6,9 +8,22 @@ app.get('/', function(req, res) {
   res.end('Analyse Meetup Attendees')
 });
 
-app.get('/api/analyse-meetup', function(req, res) {
-  res.json({
-    'attendees': []
+app.get('/api/analyze', function(req, res) {
+  var data = {
+    attendees: []
+  };
+  request('http://www.meetup.com/Docker-Copenhagen/events/220457268/', function(err, response, body) {
+    var $ = cheerio.load(body)
+    var members = $('#rsvp-list').children();
+    members.each(function(i, elem){
+      var link = $(this).find('.member-name a');
+      var member = {
+        name: link.text().trim(),
+        url: link.attr('href')
+      };
+      data.attendees.push(member);
+    });
+    res.json(data);
   });
 });
 
