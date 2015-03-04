5af0ad905a33c43ebcc00aead5186680ecc98216
Kevin Simper <kevin.simper@gmail.com>
2015-03-04 13:21:54 +0100

    Add meetup api

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.end('Analyse Meetup Atteendees')
+  res.end('Analyse Meetup Attendees')
+});
+
+app.get('/api/analyse-meetup', function(req, res) {
+  res.json({
+    'attendees': []
+  });
 });
 
 app.listen(9000);
